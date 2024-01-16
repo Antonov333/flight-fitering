861b27fc77a64c2b214ccccd1b60891c7ca8d762
Antonov333 <antonovsergei333@yahoo.com>
2024-01-16 12:03:24 +0300

    Initial commit with starting TestClasses.java and README.MD with scratch description of solution

---
+++ src/main/java/com/gridnine/testing/FlightService.java
@@ -0,0 +1,8 @@
+package com.gridnine.testing;
+
+/**
+ * <h2>FlightService</h2>
+ * Contains methods implementing criteria for filtering flights from test flight set
+ */
+public class FlightService {
+}
