afd6bb565807900d3ba5f7296288e5cdd118e337
Antonov333 <antonovsergei333@yahoo.com>
2024-01-16 13:09:46 +0300

    Made classes Flight, FlightBuilder and Segment public to enable JUnit testing and pipeline further development

--- src/main/java/com/gridnine/testing/FlightService.java
+++ src/main/java/com/gridnine/testing/FlightService.java
@@ -1,8 +1,20 @@
 package com.gridnine.testing;
 
+
+
 /**
  * <h2>FlightService</h2>
  * Contains methods implementing criteria for filtering flights from test flight set
  */
 public class FlightService {
+
+    /**
+     * <h2>totalTimeLanded</h2>
+     * @param flight nullable
+     * @return
+     */
+    public static long totalTimeLanded(Flight flight){
+        if (flight == null) {throw new IllegalArgumentException("flight must not be null");}
+        return -1L;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,3 @@
 module flight.filtering.with.tests {
+    requires org.junit.jupiter.api;
 }
